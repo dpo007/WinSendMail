8b06b9068a0b6eb219a46ef105586bb553e0bd82
DPO007 <11600782+dpo007@users.noreply.github.com>
2019-11-28 16:19:52 -0500

    Updated assembly information

--- WinSendMail/Properties/AssemblyInfo.cs
+++ WinSendMail/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WinSendMail")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sendmail replacement for use in Windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Cantankerous Apparatus")]
 [assembly: AssemblyProduct("WinSendMail")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 - Cantankerous Apparatus")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.42.0.0")]
+[assembly: AssemblyFileVersion("1.42.0.0")]
+[assembly: NeutralResourcesLanguage("en-CA")]
